1af8a0d0281cc54b7247a56a7327bbe811310a9e
Eliton Luiz <elitonluiz1989@gmail.com>
2018-10-27 15:00:01 -0400

    I created a deploy handle script.

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+main() {
+    if [ "${1}" == 'reset' ]; then
+        echo "Optimize class loader"
+        php artisan optimize
+
+        echo "Clear Cache facade value"
+        php artisan cache:clear
+
+        echo "Clear Route cache"
+        php artisan route:cache
+
+        echo "Clear View cache"
+        php artisan view:clear
+
+        echo "Clear Config cache"
+        php artisan config:cache
+    else
+        echo "Autoloader Optimization"
+        composer install --optimize-autoloader --no-dev
+
+        echo "Optimizing Configuration Loading"
+        php artisan config:cache
+
+        echo "Optimizing Route Loading"
+        php artisan route:cache
+    fi
+
+    echo "Finished!"
+}
+
+main $1
+
+exit
